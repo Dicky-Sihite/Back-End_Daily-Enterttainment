527be1f539810b90aee447817694ecf6ae2c99a6
23091397071_Dicky Dippos Sihite <149124561+Dicky-Sihite@users.noreply.github.com>
2026-06-09 03:12:53 +0700

    fitur untuk upload foto ke cloud

---
+++ src/controllers/uploadController.js
@@ -0,0 +1,45 @@
+const cloudinary = require("../config/cloudinary");
+const streamifier = require("streamifier");
+
+const uploadImage = async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({
+        message: "File tidak ditemukan",
+      });
+    }
+
+    const result = await new Promise((resolve, reject) => {
+      const stream = cloudinary.uploader.upload_stream(
+        {
+          folder: "daily-entertainment",
+        },
+        (error, result) => {
+          if (error) reject(error);
+          else resolve(result);
+        }
+      );
+
+      streamifier
+        .createReadStream(req.file.buffer)
+        .pipe(stream);
+    });
+
+    res.status(200).json({
+      success: true,
+      imageUrl: result.secure_url,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  uploadImage,
+};
